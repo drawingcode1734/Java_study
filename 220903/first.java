ceb239591b3982e45820632ccbe93046a5366266
drawingcode1734 <lljy0213@naver.com>
2022-09-05 17:46:57 +0900

    메소드 문제풀이

---
+++ 220903/first.java
@@ -0,0 +1,30 @@
+/*
+import java.util.Scanner;
+public class first {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    for(int num = 0; num < 5 ; num++ ) {
+        System.out.print("점수를 입력해 주세요 : ");
+        int score = scanner.nextInt();
+            if (score < 0)
+                System.out.println("입력한 점수의 등급은 : F");
+            else if (score <= 60)
+                System.out.println("입력한 점수의 등급은 : C");
+            else if (score <= 80)
+                System.out.println("입력한 점수의 등급은 : B");
+            else if (score <= 100)
+                System.out.println("입력한 점수의 등급은 : A");
+            else if (score > 100)
+                System.out.println("입력한 점수의 등급은 : F");
+
+        }
+
+    }
+
+
+}
+
+
+
+*/
